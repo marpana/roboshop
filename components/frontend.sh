eab86d99ddad26bac2c087b79008e85da4e29629
ArpanaM <muriki.arpana@gmail.com>
2024-11-05 15:43:12 +0530

    frontend file added

---
+++ components/frontend.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+## check whether the script is running as a root user or not
+#
+#USER_UID=$(id -u)
+#echo "$USER_UID"
+#if [ ${USER_UID} -ne 0 ]; then
+#  echo -e "\e[1;31mYou must be a root user to perform this script\e[0m"
+#  exit
+#  fi
+echo welcome to fronend script

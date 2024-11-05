9d6900bd56dc0ab01c010ed9597788bd5f59524b
ArpanaM <muriki.arpana@gmail.com>
2024-11-05 16:59:32 +0530

    frontend script added

--- components/frontend.sh
+++ components/frontend.sh
@@ -8,3 +8,19 @@
 #  exit
 #  fi
 echo welcome to fronend script
+
+ yum install nginx -y
+ systemctl enable nginx
+ systemctl start nginx
+
+ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+  cd /usr/share/nginx/html
+  rm -rf *
+  unzip /tmp/frontend.zip
+  mv frontend-main/* .
+  mv static/* .
+  rm -rf frontend-master static README.md
+  mv localhost.conf /etc/nginx/default.d/roboshop.conf
+  systemctl enable nginx
+  systemctl restart nginx

d6a85e92318e6005a69bb4bae409cf36d744910d
ArpanaM <muriki.arpana@gmail.com>
2024-11-05 15:26:18 +0530

    frontend file added

--- frontend.sh
+++ frontend.sh
@@ -4,6 +4,6 @@
 USER_UID=$(id -u)
 echo "$USER_UID"
 if [ ${USER_UID} -ne 0 ]; then
-  echo -e "\e[1 ;31mYou must be a root user to perform this script\e[0m"
+  echo -e "\e[1;31mYou must be a root user to perform this script\e[0m"
   exit
   fi
